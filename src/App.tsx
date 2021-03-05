ebc1b3b8d56d10938cddb49596221c731f021f72
Vadim Balabin <vadikfint@gmail.com>
2021-03-05 21:27:16 +0400

    feat: implement app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { PageContainer } from './components';
+import { Spin } from 'antd';
+import { useSelector } from 'react-redux';
+import { userSelector } from './store/selectors';
 
-function App() {
+const LoginPage: React.FC = () => <div>Login Page</div>;
+const LoadingPage: React.FC = () => <Spin size="large" />;
+
+const App: React.FC = () => {
+  const { isAuth, fetchingState } = useSelector(userSelector);
+  // const isAuth: boolean = true;
+  // const fetchingState: string = 'success';
+
+  const pageContent = () =>
+    fetchingState === 'loading' ? (
+      <LoadingPage />
+    ) : isAuth ? (
+      <PageContainer />
+    ) : (
+      <Redirect to="/login" />
+    );
+
   return (
-    <div className="App">
-      <h1>Hello Интенсив ТСЖ!</h1>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        background: '#f2f2f2',
+        margin: '0 auto',
+        height: '100vh',
+      }}
+    >
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route
+          exact
+          path={[
+            '/payments',
+            '/metering',
+            '/tickets/:id?',
+            '/services',
+            '/news',
+          ]}
+          component={pageContent}
+        />
+        <Route path="*">
+          <Redirect to="/payments" />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
