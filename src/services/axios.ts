c9745e827ee144977694aeec1e87662456ca5b1d
Vadim Balabin <vadikfint@gmail.com>
2021-03-08 18:23:39 +0400

    feat: add interceptors of axios for unauthorized user

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,6 +3,15 @@ import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.headers.common['Token'] = window.localStorage.getItem('Token');
 
-// window.axios = axios;
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      window.history.pushState({}, '/login');
+    }
+  }
+);
 
 export default axios;
--- src/utils/api/api.js
+++
@@ -1,6 +0,0 @@
-import { axios } from '../../services';
-
-export const userApi = {
-  auth: (post) => axios.post('/auth', post),
-  verify: () => axios.get('/auth')
-}
