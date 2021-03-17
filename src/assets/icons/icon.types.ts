c37a62725f94e3f37b9e8d64a2aa3daca1ab89da
Vadim Balabin <vadikflint@gmail.com>
2021-03-17 23:26:39 +0400

    feat: add icons

---
+++ src/assets/icons/icon.types.ts
@@ -0,0 +1,10 @@
+type Color = '#DC1C1C' | '#2B6DB0' | '#14B0F2';
+
+export const redColor: Color = '#DC1C1C';
+export const blueColor: Color = '#2B6DB0';
+export const aquaColor: Color = '#14B0F2';
+
+export type IconProps = {
+  color?: Color;
+  size?: number;
+};
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { IconExit } from './icon-exit';
+export { IconRaindrop } from './icon-raindrop';
+export { IconGas } from './icon-gas';
+export * from './icon.types';
